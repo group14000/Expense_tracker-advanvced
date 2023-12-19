bf29bc010cd174b051e68e3117da9db272cd71a1
DIGONTA HALDER <digantahalder14000@gmail.com>
2023-12-19 11:33:10 +0530

    changes

--- frontend/src/pages/expense-tracker/index.jsx
+++ frontend/src/pages/expense-tracker/index.jsx
@@ -5,11 +5,10 @@ import { useGetTransactions } from "../../hooks/useGetTransactions";
 import { useGetUserInfo } from "../../hooks/useGetUserInfo";
 import { useNavigate } from "react-router-dom";
 
-import "./styles.css";
 import { auth } from "../../config/firebase-config";
 
 export const ExpenseTracker = () => {
-  const { addTransaction } = useAddTransaction();
+  const { addTransaction, deleteTransaction } = useAddTransaction();
   const { transactions, transactionTotals } = useGetTransactions();
   const { name, profilePhoto } = useGetUserInfo();
   const navigate = useNavigate();
@@ -42,32 +41,55 @@ export const ExpenseTracker = () => {
     }
   };
 
+  const handleDelete = (id) => {
+    // Call the deleteTransaction function with the transaction id
+    deleteTransaction(id);
+  };
+
   return (
     <>
-      <div className="expense-tracker">
-        <div className="container">
-          <h1> {name}'s Expense Tracker</h1>
-          <div className="balance">
-            <h3> Your Balance</h3>
-            {balance >= 0 ? <h2> ${balance}</h2> : <h2> -${balance * -1}</h2>}
+      <div className="expense-tracker bg-gray-100 p-4 md:p-8">
+        <div className="container mx-auto">
+          <h1 className="text-2xl md:text-4xl font-bold mb-4">
+            {name}'s Expense Tracker
+          </h1>
+          <div className="balance bg-white p-4 md:p-6 rounded-md shadow-md mb-4">
+            <h3 className="text-lg md:text-xl font-semibold mb-2">
+              Your Balance
+            </h3>
+            {balance >= 0 ? (
+              <h2 className="text-2xl md:text-3xl text-green-500">
+                ${balance}
+              </h2>
+            ) : (
+              <h2 className="text-2xl md:text-3xl text-red-500">
+                -${balance * -1}
+              </h2>
+            )}
           </div>
-          <div className="summary">
-            <div className="income">
-              <h4> Income</h4>
+          <div className="summary flex gap-4 md:gap-8">
+            <div className="income bg-white p-4 md:p-6 rounded-md shadow-md flex-1">
+              <h4 className="text-lg md:text-xl font-semibold mb-2">Income</h4>
               <p>${income}</p>
             </div>
-            <div className="expenses">
-              <h4> Expenses</h4>
+            <div className="expenses bg-white p-4 md:p-6 rounded-md shadow-md flex-1">
+              <h4 className="text-lg md:text-xl font-semibold mb-2">
+                Expenses
+              </h4>
               <p>${expenses}</p>
             </div>
           </div>
-          <form className="add-transaction" onSubmit={onSubmit}>
+          <form
+            className="add-transaction bg-white p-4 md:p-6 rounded-md shadow-md mb-4"
+            onSubmit={onSubmit}
+          >
             <input
               type="text"
               placeholder="Description"
               value={description}
               required
               onChange={(e) => setDescription(e.target.value)}
+              className="mb-2 p-2 w-full"
             />
             <input
               type="number"
@@ -75,57 +97,85 @@ export const ExpenseTracker = () => {
               value={transactionAmount}
               required
               onChange={(e) => setTransactionAmount(e.target.value)}
+              className="mb-2 p-2 w-full"
             />
-            <input
-              type="radio"
-              id="expense"
-              value="expense"
-              checked={transactionType === "expense"}
-              onChange={(e) => setTransactionType(e.target.value)}
-            />
-            <label htmlFor="expense"> Expense</label>
-            <input
-              type="radio"
-              id="income"
-              value="income"
-              checked={transactionType === "income"}
-              onChange={(e) => setTransactionType(e.target.value)}
-            />
-            <label htmlFor="income"> Income</label>
-
-            <button type="submit"> Add Transaction</button>
-          </form>
-        </div>
-        {profilePhoto && (
-          <div className="profile">
-            {" "}
-            <img className="profile-photo" src={profilePhoto} />
-            <button className="sign-out-button" onClick={signUserOut}>
-              Sign Out
+            <div className="flex items-center gap-2 mb-2">
+              <input
+                type="radio"
+                id="expense"
+                value="expense"
+                checked={transactionType === "expense"}
+                onChange={(e) => setTransactionType(e.target.value)}
+              />
+              <label htmlFor="expense" className="text-sm md:text-base">
+                Expense
+              </label>
+              <input
+                type="radio"
+                id="income"
+                value="income"
+                checked={transactionType === "income"}
+                onChange={(e) => setTransactionType(e.target.value)}
+              />
+              <label htmlFor="income" className="text-sm md:text-base">
+                Income
+              </label>
+            </div>
+            <button
+              type="submit"
+              className="bg-blue-500 text-white py-2 px-4 rounded-md"
+            >
+              Add Transaction
             </button>
-          </div>
-        )}
+          </form>
+          {profilePhoto && (
+            <div className="profile flex items-center justify-between bg-white p-4 md:p-6 rounded-md shadow-md mb-4">
+              <img
+                className="profile-photo w-1/4 md:w-1/5 rounded-full"
+                src={profilePhoto}
+                alt="Profile"
+              />
+              <button
+                className="sign-out-button bg-red-500 text-white py-2 px-4 rounded-md"
+                onClick={signUserOut}
+              >
+                Sign Out
+              </button>
+            </div>
+          )}
+        </div>
       </div>
-      <div className="transactions">
-        <h3> Transactions</h3>
+      <div className="transactions bg-white p-4 md:p-8 rounded-md shadow-md">
+        <h3 className="text-2xl md:text-4xl font-bold mb-4">Transactions</h3>
         <ul>
-          {transactions.map((transaction) => {
-            const { description, transactionAmount, transactionType } =
+          {transactions.map((transaction, index) => {
+            const { id, description, transactionAmount, transactionType } =
               transaction;
             return (
-              <li>
-                <h4> {description} </h4>
-                <p>
-                  ${transactionAmount} •{" "}
-                  <label
-                    style={{
-                      color: transactionType === "expense" ? "red" : "green",
-                    }}
-                  >
-                    {" "}
-                    {transactionType}{" "}
-                  </label>
-                </p>
+              <li key={id} className="mb-2 flex justify-between items-center">
+                <div>
+                  <h4 className="text-lg md:text-xl font-semibold">
+                    {description}
+                  </h4>
+                  <p>
+                    ${transactionAmount} •{" "}
+                    <label
+                      className={`${
+                        transactionType === "expense"
+                          ? "text-red-500"
+                          : "text-green-500"
+                      }`}
+                    >
+                      {transactionType}
+                    </label>
+                  </p>
+                </div>
+                <button
+                  className="text-red-500 hover:underline"
+                  onClick={() => handleDelete(id)}
+                >
+                  Delete
+                </button>
               </li>
             );
           })}
